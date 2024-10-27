caff264be68cc9216b912f6ff5bd452b94132123
Gary <reid9000200020002000@gmail.com>
2024-10-27 10:45:32 +0800

    Add product description section

---
+++ FoodAppUI/app/src/main/java/com/example/foodappui/data/ProductDescription.kt
@@ -0,0 +1,5 @@
+package com.example.foodappui.data
+
+const val ProductDescriptionData = "Meet Mr. Burger: the ultimate burger indulgence!\n\n" +
+        "Enjoy a juicy, perfectly seasoned beef patty layered with a mouthwatering trio of melted cheddar, Swiss, and American cheeses. Topped with crispy bacon and sweet caramelized onions, each bite is a savory delight.\n\n" +
+        "Finished with a drizzle of bold BBQ sauce and served on a toasted brioche bun, Mr. Burger is the perfect combination of flavor, texture, and cheesy satisfaction."
--- FoodAppUI/app/src/main/java/com/example/foodappui/ui/screen/ProductDetailsScreen.kt
+++ FoodAppUI/app/src/main/java/com/example/foodappui/ui/screen/ProductDetailsScreen.kt
@@ -3,18 +3,21 @@ package com.example.foodappui.ui.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.foodappui.data.ProductDescriptionData
 import com.example.foodappui.data.ProductFlavorData
 import com.example.foodappui.data.ProductFlavorState
 import com.example.foodappui.data.ProductNutritionState
 import com.example.foodappui.data.ProductPreviewState
 import com.example.foodappui.data.ProductionNutritionData
 import com.example.foodappui.ui.screen.components.FlavorSection
+import com.example.foodappui.ui.screen.components.ProductDescriptionSection
 import com.example.foodappui.ui.screen.components.ProductNutritionSection
 import com.example.foodappui.ui.screen.components.ProductPreviewSection
 
@@ -23,7 +26,8 @@ fun ProductDetailsScreen(
     modifier: Modifier = Modifier,
     productPreviewState: ProductPreviewState = ProductPreviewState(),
     productFlavors: List<ProductFlavorState> = ProductFlavorData,
-    productNutritionState: ProductNutritionState = ProductionNutritionData
+    productNutritionState: ProductNutritionState = ProductionNutritionData,
+    productDescription: String = ProductDescriptionData
 ) {
     val scrollableState = rememberScrollState()
 
@@ -40,6 +44,14 @@ fun ProductDetailsScreen(
         ProductNutritionSection(
             modifier = Modifier.padding(horizontal = 18.dp),
             state = productNutritionState
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        ProductDescriptionSection(
+            modifier = Modifier
+                .navigationBarsPadding()
+                .padding(horizontal = 18.dp)
+                .padding(bottom = 128.dp),
+            productDescription = productDescription
         )
     }
 }
